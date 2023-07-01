7b9d5144f8db4ab8dfbcf84b7e0869a55498314e
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-07-01 22:37:08 +0530

    reg-form-ui

--- components/forms/mentorRegForm.js
+++ components/forms/mentorRegForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import TextBox, { SelectBox } from "../textbox";
 
 const MentorRegForm = () => {
+  const [lang, setLang] = React.useState([]);
   return (
     <>
       <main className="flex flex-col gap-2">
@@ -10,27 +11,94 @@ const MentorRegForm = () => {
             Unlock Your Potential and Inspire Others: Join Our Mentorship
             Program Today!
           </div>
-          <div className="text-base text-gray-600 max-w-[85%]">
+          <div className="text-base text-gray-600 max-w-[95%]">
             Become a beacon of inspiration and knowledge by sharing your
             expertise, experiences, and insights. As a mentor, you'll have the
             opportunity to guide and support mentees, helping them navigate
             challenges, achieve their goals, and create a brighter future
           </div>
         </main>
-        <main className="grid grid-cols-2 gap-4">
+        <main className="grid grid-cols-2 mt-6 px-4 gap-6">
           <TextBox lable={"Name"} placeholder="e.g. Rishi" />
           <TextBox lable={"Email"} placeholder="your@example.com" />
           <TextBox lable={"Contact Number"} placeholder="98XXXXXXX" />
+          <TextBox lable={"Current City"} placeholder="e.g.New Delhi" />
+          <TextBox lable={"Address"} placeholder="" />
+          <TextBox lable={"Occupation"} placeholder="e.g Software Engineer" />
+          <TextBox lable={"Address"} placeholder="" />
+
+          <SelectBox
+            list={["Male", "Female"]}
+            placeholder={"Select"}
+            lable={"Gender"}
+          />
+
+          <SelectBox
+            list={["18 - 24 years old", "25-34 years old"]}
+            placeholder={"Select"}
+            lable={"Age"}
+          />
           <SelectBox
             list={["B Tech", "BSc"]}
             placeholder={"Select"}
             lable={"Qualification"}
           />
+          <div>
+            <SelectBox
+              list={["Hindi", "English", "Marathi"]}
+              placeholder="Select your spoken languages"
+              lable={"Languages spoken"}
+              multiSelect
+              action={setLang}
+            />
+            <div className="flex gap-1">
+              {lang &&
+                lang.map((item, index) => (
+                  <span
+                    key={index}
+                    className="px-4 py-1 text-xs text-rose-950 font-bold border-2 rounded-full border-2 border-rose-900"
+                  >
+                    {item}
+                  </span>
+                ))}
+            </div>
+          </div>
+          {/* <TextBox lable={"Have you "} placeholder="" /> */}
           <SelectBox
-            list={["Hindi", "English", "Marathi"]}
-            placeholder="Select your spoken languages"
-            lable={"Languages spoken"}
+            list={["Yes", "No"]}
+            placeholder={"Select"}
+            lable={"Have you volunteered before?"}
           />
+          <SelectBox
+            list={["Yes", "No"]}
+            placeholder={"Select"}
+            lable={
+              "Are you comfortable with teaching/training/mentoring adults?"
+            }
+          />
+          <SelectBox
+            list={["Yes", "No"]}
+            placeholder={"Select"}
+            lable={
+              "Do you require a basic discussion call before session with the mentee?"
+            }
+          />
+        </main>
+        <div className="flex my-2 px-4 gap-2 w-full flex-col ">
+          <span className="block text-sm font-bold text-gray-700">
+            Is there anything else you would like us to know about your
+            availability?
+          </span>
+          <textarea
+            rows={5}
+            placeholder="Your answer"
+            className="w-full p-3 text-sm bg-gray-100 outline-none"
+          />
+        </div>
+        <main className="px-4">
+          <button className="w-full bg-cyan-700 font-medium uppercase rounded p-2 text-white ">
+            register
+          </button>
         </main>
       </main>
     </>
--- components/textbox.jsx
+++ components/textbox.jsx
@@ -17,29 +17,50 @@ const TextBox = ({ lable, action, type, placeholder }) => {
   );
 };
 
-export const SelectBox = ({ list, lable, placeholder }) => {
+export const SelectBox = ({
+  list,
+  lable,
+  placeholder,
+  action,
+  multiSelect,
+}) => {
   const [show, setShow] = useState(false);
+
+  const [value, setValue] = useState(null);
+  const [arr, setArr] = useState([]);
+  const handleSelect = (e) => {
+    if (!multiSelect) {
+      setValue(e.target.textContent);
+    } else {
+      setArr([...arr, e.target.textContent]);
+      action([...arr, e.target.textContent]);
+    }
+    setShow(false);
+  };
   return (
     <>
       <main className="flex flex-col gap-1">
-        <div className="text-gray-600">{lable}</div>
-        <div className="flex w-full flex-col gap-2">
+        <div className="text-gray-600 text-sm font-bold">{lable}</div>
+        <div className="flex relative w-full flex-col gap-2">
           <div
             onClick={() => setShow(true)}
             className="flex  bg-gray-100  justify-between p-2 rounded-lg items-center"
           >
-            <div className="">{placeholder}</div>
+            <div className="">{value ? value : placeholder}</div>
             <Image src={"/icons/down.svg"} width={20} height={20} />
           </div>
           {show && (
             <div
-              className="max-h-[200px] flex flex-col gap-1 rounded-lg border-[1px] border-gray-100"
+              className="max-h-[200px] z-[9999999] bg-white w-full absolute top-12 shadow-lg flex flex-col gap-1 rounded-lg border-[1px] border-gray-100"
               onMouseLeave={() => setShow(false)}
             >
               {list.map((item, index) => (
                 <div
                   className=" hover:bg-gray-200 p-2 hover:font-bold"
                   key={index}
+                  onClick={(e) => {
+                    handleSelect(e);
+                  }}
                 >
                   {" "}
                   {item}
@@ -49,6 +70,16 @@ export const SelectBox = ({ list, lable, placeholder }) => {
           )}
         </div>
       </main>
+    </>
+  );
+};
+
+export const Tags = ({ item, removeItem }) => {
+  return (
+    <>
+      <div className="px-2 text-xs font-medium max-w-min rounded-full py-1 flex gap-1 border-2 text-rose-900 border-rose-900">
+        {item}
+      </div>
     </>
   );
 };
